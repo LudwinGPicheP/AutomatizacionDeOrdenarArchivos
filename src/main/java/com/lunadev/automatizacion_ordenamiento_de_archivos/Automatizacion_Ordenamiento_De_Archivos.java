b9cb3337f7d8004aedd4e11cce012721a6c280f8
Luna <ludgianpichepaz@gmail.com>
2024-03-28 18:08:27 -0600

    primer commit

---
+++ src/main/java/com/lunadev/automatizacion_ordenamiento_de_archivos/Automatizacion_Ordenamiento_De_Archivos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lunadev.automatizacion_ordenamiento_de_archivos;
+
+/**
+ *
+ * @author luna
+ */
+public class Automatizacion_Ordenamiento_De_Archivos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
